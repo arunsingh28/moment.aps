989f5d192847b21eb40debe016a6ab7021980d7e
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-04 23:15:55 +0530

    exporting: getMoment function

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const date = Date.now();
-
 console.log(date)
+exports.getMoment = () => {
 
-const input_time = date - 1628098090815;
+    const input_time = date - 1628098960787;
 
-const second = Math.floor(input_time / 1000)
-
-console.log(`Min ${Math.floor(second / 60)}`)
-
-console.log(`Hours ${Math.floor(second / 3600)}`)
-
-// const minute = date.getMinutes()
-// const hours = date.getHours()
-// const year = date.getFullYear()
+    const _second = Math.floor(input_time / 1000);
+    const _hours = Math.floor(_second / 3600);
+    const _min = Math.floor(_second / 60)
 
 
 
-// console.log(minute,hours,year)
+
+    /** for seconds
+     * @logic 
+     * only for 60 seconds after 60 sec convert to min format
+    */
+
+    if (_second <= 60) {
+        console.log(`${_second} second ago`)
+    }
+    if (_second >= 60) {
+        console.log(`${_min} minute ago`)
+    }
+    if (_min >= 60) {
+        console.log(`${_hours} hours ago`)
+    }
+}
